df7ec6e0e92ddfb4e655acca044b20c207e133a9
NiackZ <niack94@gmail.com>
2021-12-26 22:48:40 +0500

    add docker, change .env & readme

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,13 +12,13 @@ import { UsersModule } from './users/users.module';
 		ConfigModule.forRoot(),
 		TypeOrmModule.forRoot({
 			type: 'postgres',
-			host: process.env.PG_HOST,
-			port: Number(process.env.PG_PORT),
-			username: process.env.PG_USER,
-			password: process.env.PG_PASS,
-			database: process.env.PG_DB,
+			host: process.env.POSTGRES_HOST,
+			port: Number(process.env.POSTGRES_PORT),
+			username: process.env.POSTGRES_USER,
+			password: process.env.POSTGRES_PASSWORD,
+			database: process.env.POSTGRES_DB,
 			entities: ["dist/models/*.entity{.ts,.js}"],
-			synchronize: false,
+			synchronize: true,
 			migrations:["dist/**/db/migrations/*{.ts,.js}"],
 			cli: {
 				"migrationsDir": "db/migrations"
